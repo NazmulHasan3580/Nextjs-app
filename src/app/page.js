6aba2f029348ab14dd5732f1484286cc38f4ada5
NazmulHasan3580 <n47712338@gmail.com>
2024-07-07 10:13:52 +0600

    second update

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
    <div>
     <h1 className="text-center mt-[10%] bg-green-800 text-white text-3xl p-4">Hello this is Nazmu hasan</h1>
+    <p>Hello Bangladesh</p>
    </div>
   );
 }
