1bb215328d50f032ae49da60327bc047430a978d
NazmulHasan3580 <n47712338@gmail.com>
2024-07-07 12:08:54 +0600

    login /signup

--- src/app/_components/restaurantSignUp.js
+++ src/app/_components/restaurantSignUp.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function RestaurantSignUp() {
   return (
-    <div className="flex justify-between items-center w-[60%]  mt-20 ">
+    <div className="flex flex-col sm:flex-row justify-between items-center sm:w-[60%] w-full mt-20 ">
         <div className="w-[350px]">
             <img src="https://indianhut.nl/wp-content/uploads/2020/05/shape.png"/>
         </div>
@@ -10,23 +10,23 @@ export default function RestaurantSignUp() {
             <h3 className="text-center text-2xl font-semibold text-secondary">SignUp Page</h3>
 
             <div className="flex flex-col justify-center items-center ">
-                <input type="text" placeholder="Enter Your Name" className="border w-[50%] min-w-[300px] outline-none p-1 text-gray-700"></input>
+                <input type="text" placeholder="Enter Your Name" className="border w-[50%] min-w-[250px] outline-none p-1 text-gray-700"></input>
             </div>
 
             <div className="flex flex-col justify-center items-center ">
-                <input type="text" placeholder="Enter Contact" className="border w-[50%] min-w-[300px] outline-none p-1 text-gray-700"></input>
+                <input type="text" placeholder="Enter Contact" className="border w-[50%] min-w-[250px] outline-none p-1 text-gray-700"></input>
             </div>
 
             <div className="flex flex-col justify-center items-center ">
-                <input type="text" placeholder="Enter your Email id" className="border w-[50%] min-w-[300px] outline-none p-1 text-gray-700"></input>
+                <input type="text" placeholder="Enter your Email id" className="border w-[50%] min-w-[250px] outline-none p-1 text-gray-700"></input>
             </div>
 
             <div className="flex flex-col justify-center items-center ">
-                <input type="password" placeholder="Enter password" className="border w-[50%] min-w-[300px] outline-none p-1 text-gray-700"></input>
+                <input type="password" placeholder="Enter password" className="border w-[50%] min-w-[250px] outline-none p-1 text-gray-700"></input>
             </div>
 
             <div className="flex flex-col justify-center items-center ">
-                <button className="border w-[50%] min-w-[300px] outline-none p-1 bg-secondary text-white mb-2">Login Now</button>
+                <button className="border w-[50%] min-w-[250px] outline-none p-1 bg-secondary text-white mb-2">Login Now</button>
             </div>
         </div>
     </div>
